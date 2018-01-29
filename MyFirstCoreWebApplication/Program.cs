21cafa8c9670cd727967410c49efd5db2af72d48
Fernando <fernando.canul@FCANUL-PC>
2018-01-29 16:10:32 -0600

    Initialize Logger in program.cs add Logger to Controller

--- MyFirstCoreWebApplication/Program.cs
+++ MyFirstCoreWebApplication/Program.cs
@@ -44,6 +44,19 @@ namespace MyFirstCoreWebApplication
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var env = hostingContext.HostingEnvironment;
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                })
                 .Build();
     }
 }
