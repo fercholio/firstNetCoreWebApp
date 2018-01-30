7a2d42d1759ae9e81091272670126ea744b1a48b
Fernando <fernando.canul@FCANUL-PC>
2018-01-30 15:48:58 -0600

    Add Angular project and configuration for use in VS. Add proxy to connect external API's

---
+++ MyFirstCoreWebApplication/API/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngularNetCoreVS.API
+{
+    [Produces("application/json")]
+    [Route("api/HelloWorld")]
+    public class HelloWorldController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Hello", "World" };
+        }
+    }
+}
